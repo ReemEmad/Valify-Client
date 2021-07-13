9808719b304d452b285ac0d2f10100af04979c3d
Reem-Emad96 <reem.emad9611@yahoo.com>
2021-07-13 14:51:44 +0200

    ⚡️ add apis

---
+++ src/apis.js
@@ -0,0 +1,41 @@
+import axios from "axios"
+import qs from "qs"
+const instance = axios.create({
+  baseURL: "http://localhost:5000",
+})
+let token = localStorage.getItem("token")
+instance.defaults.headers.common["Authorization"] = `Bearer ${token}`
+
+export let loginApi = async (obj) => {
+  let result = await instance.post("/api/login", qs.stringify(obj), {
+    headers: {
+      "content-Type": "application/x-www-form-urlencoded",
+    },
+  })
+  return result
+}
+export let verifyNID = async (obj) => {
+  let result = await instance.post("/api/upload", obj)
+  return result
+}
+
+export let facematchApi = async (obj) => {
+  let result = await instance.post("/api/uploadConfirm", obj)
+  return result
+}
+
+instance.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    console.log("error", error.response)
+    // const history = useHistory()
+    if (error && error.response.status === 401) {
+      console.log("hellooooooooo")
+      window.location.replace("/")
+      // history.push("/logout")
+    }
+    return Promise.reject(error)
+  },
+)
