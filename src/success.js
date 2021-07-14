d05ca1728fb843d58b2fb28b8977ce578fdab6c3
Reem-Emad96 <reem.emad9611@yahoo.com>
2021-07-14 03:04:30 +0200

    🐛 404 page

--- src/success.js
+++ src/success.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { Button } from "@material-ui/core"
 
 export default function Success() {
   return (
     <div>
       <header>
-        <h2>You've been verified ✅ </h2>
+        <h2>404 page </h2>
       </header>
     </div>
   )
